f5c62cea946954e835d0a37d72ed6bb2bbc77866
wanny.park <wanny.park@pandora.tv>
2024-03-28 16:06:56 +0900

    add carthage setting

---
+++ Sources/MultilineTextField/MultilineTextField.h
@@ -0,0 +1,18 @@
+//
+//  MultilineTextField.h
+//  MultilineTextField
+//
+//  Created by manaes on 3/28/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MultilineTextField.
+FOUNDATION_EXPORT double MultilineTextFieldVersionNumber;
+
+//! Project version string for MultilineTextField.
+FOUNDATION_EXPORT const unsigned char MultilineTextFieldVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MultilineTextField/PublicHeader.h>
+
+
